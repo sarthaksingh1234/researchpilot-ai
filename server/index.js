3faa90606c3291c3c66b9e1e8bc6d47396eb0d06
sarthaksingh1234 <sarthak.singh.che25@itbhu.ac.in>
2026-06-28 03:29:50 +0530

    feat: setup MERN project and connect MongoDB

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./database/connectDB");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ResearchPilot AI Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
